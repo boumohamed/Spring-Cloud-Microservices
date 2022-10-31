7fce3882bf59cb7939746371ea5f979938e28b98
boumohamed <mb.bouzri@gmail.com>
2022-10-31 22:23:39 +0100

    Eureka & dynamique Config of Gateway

--- gateway/src/main/java/me/bouzri/gateway/GatewayApplication.java
+++ gateway/src/main/java/me/bouzri/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package me.bouzri.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +15,19 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-	@Bean
+	//@Bean
 	RouteLocator  routeLocator(RouteLocatorBuilder builder)
 	{
 		return builder.routes()
-				.route(r -> r.path("/customers/**").uri("http://localhost:8082/"))
-				.route(r -> r.path("/products/**").uri("http://localhost:8083/"))
+				.route(r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+				.route(r -> r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
 				.build();
+	}
+	@Bean
+	DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient discoveryClient,
+															DiscoveryLocatorProperties properties)
+	{
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
 	}
 
 }
