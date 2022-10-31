b3d986ae7285d3e750eddc320ce59ff879daeb7b
boumohamed <mb.bouzri@gmail.com>
2022-10-31 21:53:23 +0100

    GateWay : static config (YAML, Java Class)

---
+++ gateway/src/main/java/me/bouzri/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+package me.bouzri.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	@Bean
+	RouteLocator  routeLocator(RouteLocatorBuilder builder)
+	{
+		return builder.routes()
+				.route(r -> r.path("/customers/**").uri("http://localhost:8082/"))
+				.route(r -> r.path("/products/**").uri("http://localhost:8083/"))
+				.build();
+	}
+
+}
